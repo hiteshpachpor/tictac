e7a7f704dc6afb31330f67945b904ad7a40a97c7
Hitesh Pachpor <hiteshpachpor@gmail.com>
2019-03-16 00:11:34 +0530

    Game logic written with the outcome logged to the console

--- index.js
+++ index.js
@@ -14,12 +14,65 @@
 * Winner needs to be decided and has to be flashed
 *
 * Extra points will be given for approaching the problem more creatively
-* 
+*
 */
 
 const grid = [];
 const GRID_LENGTH = 3;
 let turn = 'X';
+
+/**
+ * We will store the state of the game in a variable.
+ *
+ * 0 = Yet to start
+ * 1 = In progress
+ * 2 = Finished
+ */
+let gameState = 0;
+
+// Counter to maintain the number of turns taken
+let numberOfTurns = 0;
+
+// Handles the timeout thing before computer plays his turn
+let computerIsPlaying = false;
+
+// This sequence tells us which player won the game
+const winSequenceOfPlayers = {
+    "Player": "111",
+    "Computer": "222"
+};
+
+// Store the win routes here
+let winRoutes = [];
+
+/**
+ * There are a total of 8 combinations through which one can win:
+ * 3 rows
+ * 3 columns
+ * 2 diagonals
+ *
+ * This function creates a list of all these routes.
+ */
+(function() {
+    for (let m = 0; m < GRID_LENGTH; m++) {
+        let verticalPath = [];
+        let horizontalPath = [];
+
+        for (let n = 0; n < GRID_LENGTH; n++) {
+            verticalPath.push([m, n]);
+            horizontalPath.push([n, m]);
+        }
+
+        winRoutes.push(verticalPath);
+        winRoutes.push(horizontalPath);
+    }
+
+    leftDiagonalPath = [ [ 0, 0 ], [ 1, 1 ], [ 2, 2 ] ];
+    rightDiagonalPath = [ [ 0, 2 ], [ 1, 1 ], [ 2, 0 ] ];
+
+    winRoutes.push(leftDiagonalPath);
+    winRoutes.push(rightDiagonalPath);
+})();
 
 function initializeGrid() {
     for (let colIdx = 0;colIdx < GRID_LENGTH; colIdx++) {
@@ -33,7 +86,7 @@ function initializeGrid() {
 
 function getRowBoxes(colIdx) {
     let rowDivs = '';
-    
+
     for(let rowIdx=0; rowIdx < GRID_LENGTH ; rowIdx++ ) {
         let additionalClass = 'darkBackground';
         let content = '';
@@ -68,15 +121,39 @@ function renderMainGrid() {
     const parent = document.getElementById("grid");
     const columnDivs = getColumns();
     parent.innerHTML = '<div class="columnsStyle">' + columnDivs + '</div>';
+
+    checkIfSomeoneWon();
 }
 
 function onBoxClick() {
+    // Start the game
+    gameStarted();
+
+    // If the computer is playing his turn, don't allow player to click
+    if (computerIsPlaying) {
+        return false;
+    }
+
     var rowIdx = this.getAttribute("rowIdx");
     var colIdx = this.getAttribute("colIdx");
+
+    // If the user is clicking on an already filled cell, ignore this click
+    if (grid[colIdx][rowIdx] !== 0) {
+        return false;
+    }
+
     let newValue = 1;
     grid[colIdx][rowIdx] = newValue;
+    incrementTurns();
     renderMainGrid();
-    addClickHandlers();
+
+    // To add a more realistic feel to the game,
+    // add a small delay before the computer plays his cell.
+    computerIsPlaying = true;
+    setTimeout(function() {
+        computersTurn();
+        addClickHandlers();
+    }, 500);
 }
 
 function addClickHandlers() {
@@ -84,6 +161,116 @@ function addClickHandlers() {
     for (var idx = 0; idx < boxes.length; idx++) {
         boxes[idx].addEventListener('click', onBoxClick, false);
     }
+}
+
+/**
+ * This function plays a cell on computer's behalf
+ */
+function computersTurn() {
+    if (gameState != 1) {
+        return;
+    }
+
+    // Randomly select indexes for a cell
+    let rx = Math.round(Math.random() * (GRID_LENGTH - 1));
+    let ry = Math.round(Math.random() * (GRID_LENGTH - 1));
+
+    // If the cell is already filled, try again
+    if (grid[rx][ry] != 0) {
+        computersTurn();
+        return;
+    }
+
+    grid[rx][ry] = 2;
+
+    incrementTurns();
+    renderMainGrid();
+    computerIsPlaying = false;
+}
+
+/**
+ * This function increments the number of turns
+ */
+function incrementTurns() {
+    numberOfTurns++;
+    if (numberOfTurns == GRID_LENGTH * GRID_LENGTH) {
+        gameOver(false);
+    }
+}
+
+/**
+ * This function checks if someone has won the game
+ */
+function checkIfSomeoneWon() {
+    // Iterate over all winnable routes
+    for (let i in winRoutes) {
+        let route = winRoutes[i];
+
+        // Join all the nodes in this route to form a string
+        let currentStateOfRoute = "";
+        for (let r in route) {
+            let c = route[r];
+            currentStateOfRoute = currentStateOfRoute + "" + grid[c[0]][c[1]];
+        }
+
+        // Iterate over the players' win sequence
+        for (let player in winSequenceOfPlayers) {
+            // If the win sequence matches with any of the routes,
+            // this player has won the game
+            if (currentStateOfRoute == winSequenceOfPlayers[player]) {
+                gameOver(true, player);
+                return;
+            }
+        }
+    }
+}
+
+/**
+ * This function is called when the game is started by the user
+ */
+function gameStarted() {
+    if (gameState === 0) {
+        gameState = 1;
+    }
+}
+
+/**
+ * This function is called when the game is finished
+ * either by winning or playing out a draw
+ */
+function gameOver(gameWon, player) {
+    gameState = 2;
+    removeClickHandlers();
+
+    if (gameWon) {
+        paintWinner(player);
+    } else {
+        paintDraw();
+    }
+}
+
+/**
+ * Once the game ends, remove click handlers.
+ */
+function removeClickHandlers() {
+    var boxes = document.getElementsByClassName("box");
+    for (var idx = 0; idx < boxes.length; idx++) {
+        boxes[idx].removeEventListener('click', onBoxClick, false);
+    }
+}
+
+/**
+ * This function displays the winner
+ */
+function paintWinner(player) {
+    console.log(player);
+}
+
+/**
+ * This function displays draw
+ */
+function paintDraw() {
+    console.log("Match drawn.");
 }
 
 initializeGrid();
